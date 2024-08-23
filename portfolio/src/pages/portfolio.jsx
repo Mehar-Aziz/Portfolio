f7044013764c7fe2ff4dbea9f81d13ff6915d440
Mehar-Aziz <meharaziz.1614@gmail.com>
2024-08-23 17:56:07 +0500

    portfolio

--- portfolio/src/pages/portfolio.jsx
+++ portfolio/src/pages/portfolio.jsx
@@ -5,6 +5,7 @@ import Experience from '../components/Experience';
 import Skills from '../components/Skills';
 import Education from '../components/Education';
 import Interests from '../components/Interests';
+import Projects from '../components/Projects';
 
 const Portfolio = () => {
     return(
@@ -16,6 +17,7 @@ const Portfolio = () => {
  <Education />
  <Skills />
  <Interests />
+ <Projects />
 </div>
 
 </div>
