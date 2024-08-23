b6906ed107cc84cf8f76891ba9acf08360b406c6
Mehar-Aziz <meharaziz.1614@gmail.com>
2024-08-23 17:40:45 +0500

    Modified Interest

--- portfolio/src/components/Interests.jsx
+++ portfolio/src/components/Interests.jsx
@@ -5,8 +5,10 @@ const Interests = () => {
     <section className="resume-section p-3 p-lg-5 d-flex flex-column" id="interests">
       <div className="my-auto">
         <h2 className="mb-5">Interests</h2>
-        <p>Apart from being a software engineer, I enjoy working on technology advancements and staying updated with the latest trends in AI and machine learning.</p>
-        <p className="mb-0">In my free time, I love exploring sci-fi and fantasy movies and experimenting with new cooking techniques in the kitchen.</p>
+        <p>Beyond my work as a web developer and software engineer, I am deeply fascinated by the ongoing advancements in technology, especially in the realms of AI and machine learning. I constantly explore the latest trends and innovations in these fields, always eager to learn and apply new concepts.
+        </p>
+        <p> In addition to my tech interests, I am a huge fan of sci-fi and fantasy movies. The creativity and imagination in these genres inspire me and fuel my passion for innovation. I also enjoy experimenting in the kitchen, where I explore new cooking techniques and recipes, making the most of my culinary creativity.</p>
+        <p className="mb-0">When I'm not immersed in technology or cooking, I love spending time outdoors, whether it's hiking, biking, or simply enjoying nature. These activities provide me with balance and inspiration for both my personal and professional life.</p>
       </div>
     </section>
   );
