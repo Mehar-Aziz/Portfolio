d3e08bbf0663941f2c84343fa02cd00ea034130f
Mehar-Aziz <meharaziz.1614@gmail.com>
2024-08-23 17:36:40 +0500

    Interests component

---
+++ portfolio/src/components/Interests.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Interests = () => {
+  return (
+    <section className="resume-section p-3 p-lg-5 d-flex flex-column" id="interests">
+      <div className="my-auto">
+        <h2 className="mb-5">Interests</h2>
+        <p>Apart from being a software engineer, I enjoy working on technology advancements and staying updated with the latest trends in AI and machine learning.</p>
+        <p className="mb-0">In my free time, I love exploring sci-fi and fantasy movies and experimenting with new cooking techniques in the kitchen.</p>
+      </div>
+    </section>
+  );
+}
+
+export default Interests;
