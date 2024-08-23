67e840a3819e079938796012cf7eca34177acebd
Mehar-Aziz <meharaziz.1614@gmail.com>
2024-08-23 17:23:40 +0500

    Education in page

--- portfolio/src/pages/portfolio.jsx
+++ portfolio/src/pages/portfolio.jsx
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar';
 import About from '../components/About';
 import Experience from '../components/Experience';
 import Skills from '../components/Skills';
+import Education from '../components/Education';
 
 const Portfolio = () => {
     return(
@@ -11,6 +12,7 @@ const Portfolio = () => {
 <div className="container-fluid p-0">
  <About />
  <Experience />
+ <Education />
  <Skills />
 </div>
 
